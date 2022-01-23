da748e242fba938a797fe5cdbad9dfdbee3c0dd5
Aaron Batey <aaron@aaronbatey.com>
2022-01-23 18:50:53 -0500

    mobile view

--- src/components/shared/PageContainer/PageContainer.tsx
+++ src/components/shared/PageContainer/PageContainer.tsx
@@ -1,8 +1,19 @@
 import { Container } from "@mui/material";
 import React from "react";
+import styled from "styled-components";
+
+const ContainerFullWidthMobile = styled(Container)`
+  @media (max-width: 998px) {
+    padding: 0px;
+  }
+`;
 
 const PageContainer = ({ children }: { children: React.ReactNode }) => {
-  return <Container maxWidth="lg">{children}</Container>;
+  return (
+    <ContainerFullWidthMobile maxWidth="lg">
+      {children}
+    </ContainerFullWidthMobile>
+  );
 };
 
 export default PageContainer;
