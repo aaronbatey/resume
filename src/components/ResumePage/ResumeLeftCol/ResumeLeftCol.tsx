8c0ec8ac435952a54e4ea2e929c96014fa6139cc
Aaron Batey <aaron@aaronbatey.com>
2022-01-23 17:45:25 -0500

    add bg col color

--- src/components/ResumePage/ResumeLeftCol/ResumeLeftCol.tsx
+++ src/components/ResumePage/ResumeLeftCol/ResumeLeftCol.tsx
@@ -3,14 +3,19 @@ import ResumeContactInfo from "../ResumeContactInfo";
 import ResumePersonalDetails from "../ResumePersonalDetails";
 import ResumeSkills from "../ResumeSkills";
 import { Grid } from "@mui/material";
+import styled from "styled-components";
+
+const PrimaryGrid = styled(Grid)`
+  background: #ddf5ff;
+`;
 
 const ResumeLeftCol = () => {
   return (
-    <Grid item xs={4}>
+    <PrimaryGrid item xs={4}>
       <ResumePersonalDetails />
       <ResumeContactInfo />
       <ResumeSkills />
-    </Grid>
+    </PrimaryGrid>
   );
 };
 
--- src/components/shared/CardBorderless/CardBorderless.tsx
+++ src/components/shared/CardBorderless/CardBorderless.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 const CardBorderless = styled(Card)`
   border: none;
   box-shadow: none;
+  background: none;
 `;
 
 export default CardBorderless;
